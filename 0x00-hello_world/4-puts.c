3774833eef2fe2e12c381823d594e29155cc463f
sherrryie <sherryann478@gmail.com>
2023-03-20 12:31:46 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main . This is the main function
- * Return: Always 0 (success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include< stdio.h>
+/**
+ * main - A program that prints aline using printf function
+ * *
+ * Return: 0 (success)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);}
